0dbf41652d919b5e86f63ad867f04fe0dba77020
mechanical animal <103086203+slsww@users.noreply.github.com>
2022-05-17 14:13:53 -0700

    Create tournament-winner.java
    how to seperately update the value from different keys in a hashmap? here you go.

---
+++ tournament-winner.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Program {
+
+  public String tournamentWinner(
+      ArrayList<ArrayList<String>> competitions, ArrayList<Integer> results) {
+		String currentWinner="";
+		int i = 1;
+		Hashtable<String,Integer>ht=new Hashtable<>();
+    		for(ArrayList<String> group:competitions){
+					if(results.get(competitions.indexOf(group))==0){
+						currentWinner = group.get(1);
+						if(ht.containsKey(currentWinner)){
+							ht.put(currentWinner,(ht.get(currentWinner)+1));
+						}else{
+							ht.put(currentWinner,i);
+						}
+					}else{
+						currentWinner = group.get(0);	
+						if(ht.containsKey(currentWinner)){
+							ht.put(currentWinner,(ht.get(currentWinner)+1));
+						}else{
+							ht.put(currentWinner,i);
+						}
+					}
+				}for(Map.Entry<String,Integer> element:ht.entrySet()){
+					if(element.getValue()==Collections.max(ht.values())){
+						currentWinner = element.getKey();
+					}
+				}
+		return currentWinner;
+  }
+}
